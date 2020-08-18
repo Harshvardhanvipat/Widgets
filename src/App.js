bc2618b85e84bbaf43ded6b86a36dd37a44e4fdc
Harshvardhan <harshvardhanvipat@gmail.com>
2020-08-18 12:54:12 +1000

    Deleted files and added new custom files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Accordian from "./components/Accordion";
+import React from "react";
+import Accordion from "./components/Accordion";
 
-function App() {
+const items = [
+  {
+    title: "what is react ?",
+    content: "React is a Javascript Framework",
+  },
+  {
+    title: "Why use react ?",
+    content: "React is a Favourite Javascript library among engineers ",
+  },
+  {
+    title: "How do you use react ?",
+    content: "You use react using components",
+  },
+];
+
+export default () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Accordion items={items} />
     </div>
   );
-}
-
-export default App;
+};
